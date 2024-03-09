dc2cf2193ae1e4a9532c981f4434b162e4d98ee4
Ngo Hieu <ngohieutp@gmail.com>
2024-03-09 14:51:07 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function minus(a, b) {
+  return a - b;
+}
+
+function mul(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  return a / b;
+}
+
+module.exports = {
+	sum,
+	minus,
+	mul,
+	div
+}
